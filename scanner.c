65bdf213c2ea68c768765908ee095cb46ec99013
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-03-25 02:29:25 +1100

    16.4 Identifiers and Keywords

--- scanner.c
+++ scanner.c
@@ -21,6 +21,12 @@ InitializeScanner(const char* Source)
 }
 
 static bool
+IsAlpha(char C)
+{
+	return (C >= 'a' && C <= 'Z') || (C >= 'a' && C <= 'z') || C == '_';
+}
+
+static bool
 IsDigit(char C)
 {
 	return C >= '0' && C <= '9';
@@ -117,6 +123,82 @@ SkipWhitespace(void)
 	}
 }
 
+static token_type
+CheckKeyword(usize Start, usize Length, const char* Rest, token_type Type)
+{
+	if ((usize)(Scanner.Current - Scanner.Start) == Start + Length &&
+	        memcmp(Scanner.Start + Start, Rest, Length) == 0)
+		return Type;
+
+	return TOKEN_IDENTIFIER;
+}
+
+static token_type
+IdentifierType(void)
+{
+	switch (Scanner.Start[0])
+	{
+	case 'a':
+		return CheckKeyword(1, 2, "nd", TOKEN_AND);
+	case 'c':
+		return CheckKeyword(1, 4, "lass", TOKEN_CLASS);
+	case 'e':
+		return CheckKeyword(1, 3, "lse", TOKEN_ELSE);
+	case 'f':
+		if (Scanner.Current - Scanner.Start > 1)
+		{
+			switch (Scanner.Start[1])
+			{
+			case 'a':
+				return CheckKeyword(2, 3, "lse", TOKEN_FALSE);
+			case 'o':
+				return CheckKeyword(2, 1, "r", TOKEN_FOR);
+			case 'u':
+				return CheckKeyword(2, 1, "n", TOKEN_FUN);
+			}
+		}
+		break;
+	case 'i':
+		return CheckKeyword(1, 1, "f", TOKEN_IF);
+	case 'n':
+		return CheckKeyword(1, 2, "il", TOKEN_NIL);
+	case 'o':
+		return CheckKeyword(1, 1, "r", TOKEN_OR);
+	case 'p':
+		return CheckKeyword(1, 4, "rint", TOKEN_PRINT);
+	case 'r':
+		return CheckKeyword(1, 5, "eturn", TOKEN_RETURN);
+	case 's':
+		return CheckKeyword(1, 4, "uper", TOKEN_SUPER);
+	case 't':
+		if (Scanner.Current - Scanner.Start > 1)
+		{
+			switch (Scanner.Start[1])
+			{
+			case 'h':
+				return CheckKeyword(2, 2, "is", TOKEN_THIS);
+			case 'r':
+				return CheckKeyword(2, 2, "ue", TOKEN_TRUE);
+			}
+		}
+		break;
+	case 'v':
+		return CheckKeyword(1, 2, "ar", TOKEN_VAR);
+	case 'w':
+		return CheckKeyword(1, 4, "hile", TOKEN_WHILE);
+	}
+
+	return TOKEN_IDENTIFIER;
+}
+
+static token
+Identifier(void)
+{
+	while (IsAlpha(Peek()) || IsDigit(Peek()))
+		Advance();
+	return MakeToken(IdentifierType());
+}
+
 static token
 Number(void)
 {
@@ -164,6 +246,8 @@ ScanToken(void)
 		return MakeToken(TOKEN_EOF);
 
 	char C = Advance();
+	if (IsAlpha(C))
+		return Identifier();
 	if (IsDigit(C))
 		return Number();
 
