13d5d9245cb6c8ec8fd0d755abe7db2b2edbabb6
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-03-25 01:41:38 +1100

    16.2 A Token at a Time

--- compiler.c
+++ compiler.c
@@ -7,4 +7,22 @@ void
 Compile(const char* Source)
 {
 	InitializeScanner(Source);
+	u16 Line = -1;
+	for (;;)
+	{
+		token Token = ScanToken();
+		if (Token.Line != Line)
+		{
+			printf("%4d ", Token.Line);
+			Line = Token.Line;
+		}
+		else
+		{
+			printf("   | ");
+		}
+		printf("%2d '%.*s'\n", Token.Type, Token.Length, Token.Start);
+
+		if (Token.Type == TOKEN_EOF)
+			break;
+	}
 }
--- scanner.c
+++ scanner.c
@@ -19,3 +19,42 @@ InitializeScanner(const char* Source)
 	Scanner.Current = Source;
 	Scanner.Line = 1;
 }
+
+static bool
+IsAtEnd(void)
+{
+	return *Scanner.Current == '\0';
+}
+
+static token
+MakeToken(token_type Type)
+{
+	token Token;
+	Token.Type = Type;
+	Token.Start = Scanner.Start;
+	Token.Length = (u16)(Scanner.Current - Scanner.Start);
+	Token.Line = Scanner.Line;
+	return Token;
+}
+
+static token
+ErrorToken(const char* Message)
+{
+	token Token;
+	Token.Type = TOKEN_ERROR;
+	Token.Start = Message;
+	Token.Length = (u16)strlen(Message);
+	Token.Line = Scanner.Line;
+	return Token;
+}
+
+token
+ScanToken(void)
+{
+	Scanner.Start = Scanner.Current;
+
+	if (IsAtEnd())
+		return MakeToken(TOKEN_EOF);
+
+	return ErrorToken("Unexpected character.");
+}
