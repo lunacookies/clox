b107dbcfcd0232428e4c18aca97fef5388ec3782
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-10-12 21:42:07 +1100

    14.4 Disassembling Chunks

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
+#include "chunk.h"
 #include "common.h"
+#include "debug.h"
 
 int main(int argc, char *argv[]) {
+	chunk chunk;
+	initChunk(&chunk);
+	writeChunk(&chunk, OP_RETURN);
+
+	disassembleChunk(&chunk, "test chunk");
+	freeChunk(&chunk);
+
 	return 0;
 }
